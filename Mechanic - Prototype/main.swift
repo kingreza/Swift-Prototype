af023191b8d3f6a01a4b2e4c1b19c9d7dbe8eb55
Reza Shirazian <reza.shirazian@gmail.com>
2016-05-02 23:21:05 -0700

    Initial Commit

---
+++ Mechanic - Prototype/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Mechanic - Prototype
+//
+//  Created by Reza Shirazian on 2016-05-02.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
